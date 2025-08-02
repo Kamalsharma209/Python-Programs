5eafa3f2c84343b88b58a72817774e68b442139f
Your Name <Kamalsharma4769@gmail.com>
2025-08-02 10:51:54 +0530

    Scenrio Based Problem

---
+++ Scenior based on Dict.py
@@ -0,0 +1,30 @@
+subjects = {
+    "python","java","c++","python","javascript","java","python","java","c++","c"
+}
+# sets is for stoing unique elements
+print(len(subjects))
+
+
+'''You are given a list of subjects for students. Assume one classroom is required for 1
+subject. How many classrooms are needed by all students.
+
+”python”
+,
+“java”
+,
+“C++”
+,
+“python”
+,
+“javascript”
+,
+
+“java”
+,
+“python”
+,
+“java”
+,
+“C++”
+,
+“C”'''
